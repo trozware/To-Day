19af895c97beed2de6ec1b95fbea49f84c0f2a6c
Sarah Reichelt <sarah@troz.net>
2023-01-16 08:49:12 +1000

    Initial Commit

---
+++ Today/TodayApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodayApp.swift
+//  Today
+//
+//  Created by Sarah Reichelt on 16/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct TodayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
